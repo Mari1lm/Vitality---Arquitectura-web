c3f39f660f56b1deef57aa2c49f133f4ff253234
Diegogh112 <u202217814@upc.edu.pe>
2024-04-11 12:41:26 -0500

    .

--- Vitality/src/main/java/com/example/vitality/repositories/IRecommendationRepository.java
+++ Vitality/src/main/java/com/example/vitality/repositories/IRecommendationRepository.java
@@ -9,4 +9,5 @@ import java.util.List;
 @Repository
 public interface IRecommendationRepository extends JpaRepository<Recommendation,Integer> {
 
+    List<String[]> quantityRecommendationByMovie();
 }

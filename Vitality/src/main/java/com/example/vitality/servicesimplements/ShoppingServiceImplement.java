d106e3c711213d700b93a002fa322e0ab42636c6
Michelalcc <166627951+Michelalcc@users.noreply.github.com>
2024-04-15 11:21:19 -0500

    Add files via upload

---
+++ Vitality/src/main/java/com/example/vitality/servicesimplements/ShoppingServiceImplement.java
@@ -0,0 +1,24 @@
+package com.example.vitality.servicesimplements;
+import com.example.vitality.entities.Shopping;
+import com.example.vitality.repositories.IShoppingRepository;
+import com.example.vitality.servicesinterfaces.IShoppingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ShoppingServiceImplement implements IShoppingService {
+    @Autowired
+    private IShoppingRepository sR;
+    @Override
+    public void insert(Shopping shopping) {
+        sR.save(shopping);
+    }
+
+    @Override
+    public List<Shopping> list() {
+        return sR.findAll();
+    }
+
+}

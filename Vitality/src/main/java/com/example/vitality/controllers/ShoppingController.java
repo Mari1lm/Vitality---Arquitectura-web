01cddf214985ae823d7db0bc7c98e4302e0dd38a
Diegogh112 <u202217814@upc.edu.pe>
2024-04-24 12:02:39 -0500

    fas

--- Vitality/src/main/java/com/example/vitality/controllers/ShoppingController.java
+++ Vitality/src/main/java/com/example/vitality/controllers/ShoppingController.java
@@ -13,7 +13,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Service
+@RestController
+@RequestMapping("/compras")
 public class ShoppingController {
     @Autowired
     private IShoppingService sS;

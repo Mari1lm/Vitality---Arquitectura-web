87e5588868434f4173aa2bb7ac78050c10590bd1
Diegogh112 <u202217814@upc.edu.pe>
2024-04-26 18:32:12 -0500

    a

--- Vitality/src/main/java/com/example/vitality/controllers/UserController.java
+++ Vitality/src/main/java/com/example/vitality/controllers/UserController.java
@@ -18,94 +18,84 @@ public class UserController {
     private IUserService uS;
 
     @PostMapping
-    public void insertar(@RequestBody UserDTO userDTO){
-        ModelMapper d= new ModelMapper();
-        User user=d.map(userDTO,User.class);
+    public void insertar(@RequestBody UserDTO userDTO) {
+        ModelMapper d = new ModelMapper();
+        User user = d.map(userDTO, User.class);
         uS.insert(user);
     }
 
     @PutMapping("/modificar")
-    public void modificar(@RequestBody UserDTO userDTO){
-        ModelMapper d= new ModelMapper();
-        User user=d.map(userDTO,User.class);
+    public void modificar(@RequestBody UserDTO userDTO) {
+        ModelMapper d = new ModelMapper();
+        User user = d.map(userDTO, User.class);
         uS.insert(user);
     }
 
 
     @GetMapping("/listar-usuarios")
-    public List<UserDTO> listar(){
-        return uS.list().stream().map(y->{
-            ModelMapper m=new ModelMapper();
-            return m.map(y,UserDTO.class);
-        }).collect(Collectors.toList());
-    }
-
-
-    @DeleteMapping("/{id}")
-    public void eliminar(@PathVariable("id") Integer id){
-        uS.delete(id);
-    }
-
-    @GetMapping("/{id} listar")
-    public UserDTO listarId(@PathVariable("id") Integer id){
-
-        ModelMapper m= new ModelMapper();
-        UserDTO dto=m.map(uS.listId(id),UserDTO.class);
-        return dto;
-
-    }
-
-    @GetMapping("/finda")
-    public List<UserDTO> buscar(@RequestParam Float peso){
-        return uS.findByWeight(peso).stream().map(y->{
-            ModelMapper m=new ModelMapper();
-            return m.map(y,UserDTO.class);
-        }).collect(Collectors.toList());
-
-    }
-
-
-    @GetMapping("/findb")
-    public List<UserDTO> buscar(@RequestParam String suscripcion) {
-        return uS.findBySubscription(suscripcion).stream().map(y -> {
+    public List<UserDTO> listar() {
+        return uS.list().stream().map(y -> {
             ModelMapper m = new ModelMapper();
             return m.map(y, UserDTO.class);
         }).collect(Collectors.toList());
     }
 
+
+    @DeleteMapping("/{id}")
+    public void eliminar(@PathVariable("id") Integer id) {
+        uS.delete(id);
+    }
+
+    @GetMapping("/{id} listar")
+    public UserDTO listarId(@PathVariable("id") Integer id) {
+
+        ModelMapper m = new ModelMapper();
+        UserDTO dto = m.map(uS.listId(id), UserDTO.class);
+        return dto;
+
+    }
+
+    @GetMapping("/findbyweight")
+    public List<UserDTO> buscar(@RequestParam Float peso) {
+        return uS.findByWeight(peso).stream().map(y -> {
+            ModelMapper m = new ModelMapper();
+            return m.map(y, UserDTO.class);
+        }).collect(Collectors.toList());
+
+    }
+
+
     @GetMapping("/findbysuscription")
-    public List<UserDTO> buscarporsuscripcion(@RequestParam String suscripcion){
-        return uS.findBySubscription(suscripcion).stream().map(y->{
-            ModelMapper m=new ModelMapper();
-            return m.map(y,UserDTO.class);
+    public List<UserDTO> buscarporsuscripcion(@RequestParam String suscripcion) {
+        return uS.findBySubscription(suscripcion).stream().map(y -> {
+            ModelMapper m = new ModelMapper();
+            return m.map(y, UserDTO.class);
         }).collect(Collectors.toList());
 
     }
 
     @GetMapping("/findbyprofessional")
-    public List<UserDTO> buscarporprofesional(@RequestParam String profesional){
-        return uS.findByHealthProfessional(profesional).stream().map(y->{
-            ModelMapper m=new ModelMapper();
-            return m.map(y,UserDTO.class);
+    public List<UserDTO> buscarporprofesional(@RequestParam String profesional) {
+        return uS.findByHealthProfessional(profesional).stream().map(y -> {
+            ModelMapper m = new ModelMapper();
+            return m.map(y, UserDTO.class);
         }).collect(Collectors.toList());
 
     }
-
 
 
     @GetMapping("/Totalproductoscompradosportipo")
     public List<UserBySumProductsByTypeDTO> sumaproductosportipo(@RequestParam String Type) {
 
         List<String[]> filaLista = uS.finUserSumProductsByType(Type);
-        List<UserBySumProductsByTypeDTO> dtoLista=new ArrayList<>();
-        for(String[] columna:filaLista){
-            UserBySumProductsByTypeDTO dto=new UserBySumProductsByTypeDTO();
+        List<UserBySumProductsByTypeDTO> dtoLista = new ArrayList<>();
+        for (String[] columna : filaLista) {
+            UserBySumProductsByTypeDTO dto = new UserBySumProductsByTypeDTO();
             dto.setNameUser(columna[0]);
             dto.setSumProdByType(Integer.parseInt(columna[1]));
             dtoLista.add(dto);
         }
-        return  dtoLista;
+        return dtoLista;
+
     }
-
-
 }

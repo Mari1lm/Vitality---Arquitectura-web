74d837f5a8de432009974e9d658a9afa500457df
Mari1lm <cabrejosrojasmaria@gmail.com>
2024-04-27 03:56:39 -0500

    corrección shopping controller

--- Vitality/src/main/java/com/example/vitality/controllers/ShoppingController.java
+++ Vitality/src/main/java/com/example/vitality/controllers/ShoppingController.java
@@ -1,25 +1,25 @@
 package com.example.vitality.controllers;
 
-//import com.example.vitality.dtos.ShoppingDTO;
-//import com.example.vitality.entities.Shopping;
-//import com.example.vitality.servicesinterfaces.IShoppingService;
+import com.example.vitality.dtos.ShoppingDTO;
+import com.example.vitality.entities.Shopping;
+import com.example.vitality.servicesinterfaces.IShoppingService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.stream.Collectors;
-
+@RestController
+@RequestMapping("/compras")
 public class ShoppingController {
-    /*@Autowired
+    @Autowired
     private IShoppingService sS;
 
     @PostMapping
     public void insertar(@RequestBody ShoppingDTO shoppingDTO) {
         ModelMapper d = new ModelMapper();
         Shopping shopping = d.map(shoppingDTO, Shopping.class);
-        sS.insert(movie);
+        sS.insert(shopping);
     }
 
     @GetMapping
@@ -28,5 +28,5 @@ public class ShoppingController {
             ModelMapper m=new ModelMapper();
             return  m.map(y,ShoppingDTO.class);
         }).collect(Collectors.toList());
-    }*/
+    }
 }

600e4b6c3305c7e3901efe6d043197816d6f8595
Diegogh112 <u202217814@upc.edu.pe>
2024-06-18 11:39:36 -0500

    backup

--- Vitality/src/main/java/com/example/vitality/controllers/AnswerController.java
+++ Vitality/src/main/java/com/example/vitality/controllers/AnswerController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
-
+//aaaa
 @RestController
 @RequestMapping("/Respuesta")
 public class AnswerController {
